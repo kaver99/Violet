5eaafdab067c097c63f5c9909f2e619ca962cee9
minsungkim <minsungkim@172.30.1.46>
2020-02-13 17:43:43 +0900

    [20200213][메인 리스트 디자인 수정 및 로그인 디자인 수정, 회원가입 디자인 및 validation 수정]

--- VioletWeb/src/main/resources/static/js/signup/validation.js
+++ VioletWeb/src/main/resources/static/js/signup/validation.js
@@ -17,82 +17,210 @@ function certification_validation() {
 /*[- --------------------------------------------------------- -]*/
 /*[- STEP2 Validation Check -]*/
 function agreement_validation() {
-	var status = "";
-	var content = "";
+//	var content = "";
 	if($("#terms-of-service-chkbox").prop("checked") === false) {
-		status = "error";
-		content = "[VIOLET 이용 약관] 는 필수 체크 항목입니다.";
+//		content = "[VIOLET 이용 약관] 는 필수 체크 항목입니다.";
+		$("#terms-of-service-chkbox ~ b.form-text").css("margin-bottom", ".1rem");
+		$("#terms-of-service-chkbox ~ b.form-text").css("border-bottom", "2px dashed #f4361e");
+		$("#terms-of-service-chkbox").focus();
+		return false;
 	} else if($("#personal-information-chkbox").prop("checked") === false) {
-		status = "error";
-		content = "[VIOLET 개인정보 수집 맟 아용에 대한 안내] 는 필수 체크 항목입니다.";
+//		content = "[VIOLET 개인정보 수집 맟 아용에 대한 안내] 는 필수 체크 항목입니다.";
+		$("#personal-information-chkbox ~ b.form-text").css("margin-bottom", ".1rem");
+		$("#personal-information-chkbox ~ b.form-text").css("border-bottom", "2px dashed #f4361e");
+		$("#personal-information-chkbox").focus();
+		return false;
 	} else {
-		status = "success";
+		$("#terms-of-service-chkbox ~ b.form-text").css("margin-bottom", "0");
+		$("#terms-of-service-chkbox ~ b.form-text").css("border-bottom", "none");
+		$("#personal-information-chkbox ~ b.form-text").css("margin-bottom", "0");
+		$("#personal-information-chkbox ~ b.form-text").css("border-bottom", "none");
+		
 		$("#terms-of-service-chkbox").val('Y');
 		$("#personal-information-chkbox").val('Y');
 		if($("#event-alarm-chkbox").prop("checked") === true) {
 			$("#event-alarm-chkbox").val('Y');
 		}
+		return true;
 	}
 	/*[- alert Active Event -]*/
-	return alertCallEvent(status, content);
+//	return alertCallEvent(status, content);
 }
 
 /*[- --------------------------------------------------------- -]*/
 /*[- STEP3 Validation Check -]*/
-function details_validation() {
-	var status = "";
-	var content = "";
-	var inputId = "";
-	var pwRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,20}/;
-	var phoneRegExp = /(01[016789])([0-9]{1}[0-9]{2,3})([0-9]{4})$/;
-	
-	if($('#username').val().length < 3 || $('#username').val().length > 50) {
-		status = "error";
-		content = "이메일을 50자 이내로 작성하여 주세요.";
-//		inputId = $('#username').attr("id");
-		$('#username').focus();
-	} else if($('#usernameInput').hasClass("has-danger")) {
-		status = "error";
-		content = "잘못된 이메일 형식이거나 이미 등록된 사용자 입니다.";
-//		inputId = $('#username').attr("id");
-		$('#username').focus();
-	} else if(!$("#name").val() || $("#name").val().length > 20) {
-		status = "error";
-		content = "이름을 20자 이내로 작성하여 주세요.";
-//		inputId = $('#name').attr("id");
-		$('#name').focus();
-	} else if((!pwRegExp.test($("#password").val()))) {
-		status = "error";
-		content = "패스워드를 20자 이하, 앞자리 대문자, 하나 이상의 숫자 및 특수 문자 포함하여 작성하여 주세요.";
-//		inputId = $('#password').attr("id");
-		$('#password').focus();
-	} else if($("#password2").val().length < 8 || $("#password2").val().length > 20) {
-		status = "error";
-		content = "패스워드 확인을 20자 이내로 작성하여 주세요.";
-//		inputId = $('#password2').attr("id");
-		$('#password2').focus();
-	} else if($("#password2").val() != $("#password").val()) {
-		status = "error";
-		content = "패스워드 확인의 패스워드와 기존 작성한 패스워드가 불일치 합니다.";
-//		inputId = $('#password2').attr("id");
-		$('#password2').focus();
-	} else if($("#phone").val().length > 20 || !$("#phone").val() || !phoneRegExp.test($("#phone").val())) {
-		status = "error";
-		content = "휴대폰 번호를 20자 이내로 작성하여 주세요.";
-//		inputId = $('#phone').attr("id");
-		$('#phone').focus();
-	} else if(!$("#telecom").val()) {
-		status = "error";
-		content = "통신사를 선택하여 주세요.";
-//		inputId = $('#phone').attr("id");
+var invalid = [];
+
+function checkId() {
+	var username = $('#username').val();
+	var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+	if(regExp.test(username)) {
+		setTimeout(function () {
+			$.ajax({
+				url: '/signup/userChk.violet'
+				,type: 'post'
+				,data: {username:username}
+				,success: function(data) {
+					if($.trim(data) == 0) {
+						input_focus('username', 'success', '');
+//						$('label[for=username]').nextAll('.input-help').html('');
+//						$('label[for=username] > .input-border').css('transform', 'scaleX(1)');
+//						$('label[for=username] > .input-border').css('background', '#28a745');
+//						$('label[for=username] > .input-label').css('color', '#28a745');
+//						$('label[for=username] + .input-help').css('color', '#A9A9A9');
+					} else {
+						content = "* 등록되어 있는 이메일 입니다. 다른 이메일로 작성해 주세요.";
+						input_focus('username', 'error', content);
+//						$('label[for=username]').nextAll('.input-help').html(content);
+//						$('label[for=username] > .input-border').css('transform', 'scaleX(0)');
+//						$('label[for=username] > .input-border').css('background', '#f4361e');
+//						$('label[for=username] > .input-label').css('color', '#f4361e');
+//						$('label[for=username] + .input-help').css('color', '#f4361e');
+//						$('#username').focus();
+					}
+				}
+				,error: function(msg) {
+// 					$('.modal-domain').add('<div th:replace="menu/modal :: modal" th:remove="tag"></div>');
+				}
+			})
+		}, 500);
+		
 	} else {
-		status = "success";
+		content = "* 잘못된 형식의 이메일 입니다. 다시 입력해 주세요."
+		input_focus('username', 'error', content);
+//		$('label[for=username]').nextAll('.input-help').html(content);
+//		$('label[for=username] > .input-border').css('transform', 'scaleX(0)');
+//		$('label[for=username] > .input-border').css('background', '#f4361e');
+//		$('label[for=username] > .input-label').css('color', '#f4361e');
+//		$('label[for=username] + .input-help').css('color', '#f4361e');
+//		$('#username').focus();
 	}
-	/*[- input Focus Event -]*/
-//	input_focus(inputId, status);
+}
+
+function checkName() {
+	var content = "";
+	
+	if($("#name").val().length < 1 || $("#name").val().length > 20) {
+		content = "* 이름을 20자 이내로 작성하여 주세요.";
+		input_focus('name', 'error', content);
+//		$('label[for=name]').nextAll('.input-help').html(content);
+//		$('label[for=name] > .input-border').css('background', '#f4361e');
+//		$('label[for=name] > .input-label').css('color', '#f4361e');
+//		$('label[for=name] + .input-help').css('color', '#f4361e');
+//		$('#name').focus();
+		return false;
+	} else {
+		input_focus('name', 'success', '');
+//		$('label[for=name]').nextAll('.input-help').html('');
+//		$('label[for=name] > .input-border').css('background', '#28a745');
+//		$('label[for=name] > .input-label').css('color', '#28a745');
+//		$('label[for=name] + .input-help').css('color', '#A9A9A9');
+		return true;
+	}
+}
+
+function checkPassword() {
+	var content = "";
+	var pwRegExp = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+	
+	if($("#password").val().length < 8 || $("#password").val().length > 15) {
+		content = "* 암호를 최소 8자 이상 최대 15자 이내로 작성하여 주세요.";
+		input_focus('password', 'error', content);
+//		$('label[for=password]').nextAll('.input-help').html(content);
+//		$('label[for=password] > .input-border').css('background', '#f4361e');
+//		$('label[for=password] > .input-label').css('color', '#f4361e');
+//		$('label[for=password] + .input-help').css('color', '#f4361e');
+//		$('#password').focus();
+		return false;
+		
+	} else if((!pwRegExp.test($("#password").val()))) {
+		content = "* 암호를 하나 이상의 숫자 및 특수 문자를 포함하여 작성하여 주세요.";
+		input_focus('password', 'error', content);
+//		$('label[for=password]').nextAll('.input-help').html(content);
+//		$('label[for=password] > .input-border').css('background', '#f4361e');
+//		$('label[for=password] > .input-label').css('color', '#f4361e');
+//		$('label[for=password] + .input-help').css('color', '#f4361e');
+//		$('#password').focus();
+		return false;
+		
+	} else {
+		input_focus('password', 'success', '');
+//		$('label[for=password]').nextAll('.input-help').html('');
+//		$('label[for=password] > .input-border').css('background', '#28a745');
+//		$('label[for=password] > .input-label').css('color', '#28a745');
+//		$('label[for=password] + .input-help').css('color', '#A9A9A9');
+		return true;
+	}
+}
+
+function checkPassword2() {
+	var content = "";
+	
+	if($("#passwordChk").val() != $("#password").val()) {
+		content = "* 암호 확인란의 내용과 기존 작성한 암호의 내용이 불일치 합니다.";
+		input_focus('passwordChk', 'error', content);
+//		$('label[for=passowrdChk]').nextAll('.input-help').html(content);
+//		$('label[for=passwordChk] > .input-border').css('background', '#f4361e');
+//		$('label[for=passwordChk] > .input-label').css('color', '#f4361e');
+//		$('label[for=passwordChk] + .input-help').css('color', '#f4361e');
+//		$('#passwordChk').focus();
+		return false;
+		
+	} else {
+		input_focus('passwordChk', 'success', '');
+//		$('label[for=passwordChk]').nextAll('.input-help').html('');
+//		$('label[for=passwordChk] > .input-border').css('background', '#28a745');
+//		$('label[for=passwordChk] > .input-label').css('color', '#28a745');
+//		$('label[for=passwordChk] + .input-help').css('color', '#A9A9A9');
+		return true;
+	}
+}
+
+$("#phone").inputmask("999-999[9]-9999",{ placeholder:" ", clearMaskOnLostFocus: true,  keepStatic: true });
+function checkPhone() {
+	var content = "";
+	var _phoneNum = $("#phone").val();
+	var phoneRegExp = /(01[016789])-([0-9]{1}[0-9]{2,3})-([0-9]{4})$/;
+	
+	if(_phoneNum.length < 11 || _phoneNum.length > 14 || phoneRegExp.test(_phoneNum) == false) {
+		content = "휴대폰 번호를 13자리 이내로 작성하여 주세요.";
+		input_focus('phone', 'error', content);
+//		$('label[for=phone]').nextAll('.input-help').html(content);
+//		$('label[for=phone] > .input-border').css('background', '#f4361e');
+//		$('label[for=phone] > .input-label').css('color', '#f4361e');
+//		$('label[for=phone] + .input-help').css('color', '#f4361e');
+//		$('#phone').focus();
+		return false;
+		
+	} else {
+		input_focus('phone', 'success', '');		
+//		$('label[for=phone]').nextAll('.input-help').html('');
+//		$('label[for=phone] > .input-border').css('background', '#28a745');
+//		$('label[for=phone] > .input-label').css('color', '#28a745');
+//		$('label[for=phone] + .input-help').css('color', '#A9A9A9');
+		return true;
+	}
+}
+
+function details_validation() {
+	checkId();
+	if(document.activeElement.id == 'username') {
+		return false;
+	} else if(checkName() == false ){
+		return false;
+	} else if(checkPassword() == false) {
+		return false;
+	} else if(checkPassword2() == false) {
+		return false;
+	} else if(checkPhone() == false) {
+		return false;
+	} else {
+		return true;
+	}
 	
 	/*[- alert Active Event -]*/
-	return alertCallEvent(status, content);
+//	return alertCallEvent(status, content);
 }
 
 /*[- --------------------------------------------------------- -]*/
@@ -106,21 +234,21 @@ function address_validation() {
 }
 
 /*[- input Focus Event -]*/
-function input_focus(_inputId, _status) {
-	var $inputId = $('#' + _inputId);
-	var $inputParentId = $inputId.parent().attr("id");
+function input_focus(_inputId, _status, _content) {
 	if(_status === "error") {
-		if($('#' + $inputParentId).hasClass("has-success")) {
-			$('#' + $inputParentId).removeClass("has-success");
-		}
-		$('#' + $inputParentId).addClass("has-danger");
-		$('#' + $inputParentId).find('.form-control-feedback').html('<i class="material-icons">clear</i>');
-		$('#' + $inputParentId).find('input').focus();
+		/*[- input Focus ERROR -]*/
+		$('label[for='+_inputId+']').nextAll('.input-help').html(_content);
+		$('label[for='+_inputId+'] > .input-border').css('transform', 'scaleX(0)');
+		$('label[for='+_inputId+'] > .input-border').css('background', '#f4361e');
+		$('label[for='+_inputId+'] > .input-label').css('color', '#f4361e');
+		$('label[for='+_inputId+'] + .input-help').css('color', '#f4361e');
+		$('#'+_inputId).focus();
 	} else {
-		if($('#' + $inputParentId).hasClass("has-danger")) {
-			$('#' + $inputParentId).removeClass("has-danger");
-		}
-		$('#' + $inputParentId).addClass("has-success");
-		$('#' + $inputParentId + '> .form-control-feedback').html('<i class="material-icons">done</i>');
+		/*[- input Focus SUCCESS -]*/
+		$('label[for='+_inputId+']').nextAll('.input-help').html('');
+		$('label[for='+_inputId+'] > .input-border').css('transform', 'scaleX(1)');
+		$('label[for='+_inputId+'] > .input-border').css('background', '#28a745');
+		$('label[for='+_inputId+'] > .input-label').css('color', '#28a745');
+		$('label[for='+_inputId+'] + .input-help').css('color', '#A9A9A9');
 	}
 }
